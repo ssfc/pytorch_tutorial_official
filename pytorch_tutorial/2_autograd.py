40ad1e400a359506ac4f2fe37113e1c88fffd061
YE GAO <sortsimilar@gmail.com>
2021-01-12 16:49:25 +0800

    Update 2_autograd.py

--- pytorch_tutorial/2_autograd.py
+++ pytorch_tutorial/2_autograd.py
@@ -1,3 +1,6 @@
+# https://pytorch.org/tutorials/beginner/blitz/autograd_tutorial.html
+
+
 import torch, torchvision
 
 model = torchvision.models.resnet18(pretrained=True)
@@ -11,6 +14,32 @@ loss.backward() # backward pass
 optim = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9)
 
 optim.step() #gradient descent
+
+# Differentiation in Autograd
+a = torch.tensor([2., 3.], requires_grad=True)
+b = torch.tensor([6., 4.], requires_grad=True)
+
+Q = 3*a**3 - b**2
+
+external_grad = torch.tensor([1., 1.])
+Q.backward(gradient=external_grad)
+
+# check if collected gradients are correct
+print(9*a**2 == a.grad)
+print(-2*b == b.grad)
+
+x = torch.rand(5, 5)
+y = torch.rand(5, 5)
+z = torch.rand((5, 5), requires_grad=True)
+
+a = x + y
+print(f"Does `a` require gradients? : {a.requires_grad}")
+b = x + z
+print(f"Does `b` require gradients?: {b.requires_grad}")
+
+
+
+
 
 
 
