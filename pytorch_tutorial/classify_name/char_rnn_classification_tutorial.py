cc1407649f5c6aac00a9a93c05df65085616bc36
YE GAO <sortsimilar@gmail.com>
2021-06-14 20:06:09 +0800

    Update char_rnn_classification_tutorial.py

--- pytorch_tutorial/classify_name/char_rnn_classification_tutorial.py
+++ pytorch_tutorial/classify_name/char_rnn_classification_tutorial.py
@@ -44,15 +44,15 @@ all_categories = []
 
 
 # Read a file and split into lines
-def read_lines(filename):
-    lines = open(filename, encoding='utf-8').read().strip().split('\n')
+def read_lines(file_name):
+    lines = open(file_name, encoding='utf-8').read().strip().split('\n')
     return [unicode_to_ascii(line) for line in lines]
 
 
-for filename in find_files('data/names/*.txt'):
-    category = os.path.splitext(os.path.basename(filename))[0]
+for file_name in find_files('data/names/*.txt'):
+    category = os.path.splitext(os.path.basename(file_name))[0]
     all_categories.append(category)
-    lines = read_lines(filename)
+    lines = read_lines(file_name)
     category_lines[category] = lines
 
 n_categories = len(all_categories)
