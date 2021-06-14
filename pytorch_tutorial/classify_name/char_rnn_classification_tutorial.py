28d3cfdea49b99c2f5f78ed0e86e27465c71931c
YE GAO <sortsimilar@gmail.com>
2021-06-14 11:48:42 +0800

    Update char_rnn_classification_tutorial.py

--- pytorch_tutorial/classify_name/char_rnn_classification_tutorial.py
+++ pytorch_tutorial/classify_name/char_rnn_classification_tutorial.py
@@ -86,14 +86,14 @@ print(category_lines['Italian'][:5])
 
 
 # Find letter index from all_letters, e.g. "a" = 0
-def letterToIndex(letter):
+def letter_to_index(letter):
     return all_letters.find(letter)
 
 
 # Just for demonstration, turn a letter into a <1 x n_letters> Tensor
 def letterToTensor(letter):
     tensor = torch.zeros(1, n_letters)
-    tensor[0][letterToIndex(letter)] = 1
+    tensor[0][letter_to_index(letter)] = 1
     return tensor
 
 
@@ -102,7 +102,7 @@ def letterToTensor(letter):
 def lineToTensor(line):
     tensor = torch.zeros(len(line), 1, n_letters)
     for li, letter in enumerate(line):
-        tensor[li][0][letterToIndex(letter)] = 1
+        tensor[li][0][letter_to_index(letter)] = 1
     return tensor
 
 
