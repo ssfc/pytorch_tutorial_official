90fbc1cb9a1ca1f011f722066fdd0e3a01dcc7a4
YE GAO <sortsimilar@gmail.com>
2022-01-21 22:09:49 +0800

    update

--- 1Introduction_to_pyTorch/tensors/tensor.py
+++ 1Introduction_to_pyTorch/tensors/tensor.py
@@ -1,5 +1,22 @@
 # https://pytorch.org/tutorials/beginner/basics/tensorqs_tutorial.html
 
+import torch
+import numpy as np
+
+#######################################################################################################
+# 1: Initializing a Tensor
+# Directly from data
+data = [[1, 2],[3, 4]]
+x_data = torch.tensor(data)
+
+# From a NumPy array
+np_array = np.array(data)
+x_np = torch.from_numpy(np_array)
+
+
+
+
+
 
 
 
