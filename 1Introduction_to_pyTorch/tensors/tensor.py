82cf886e06157b87eb0792f16999e82f40337af9
YE GAO <sortsimilar@gmail.com>
2022-01-21 23:03:22 +0800

    Update tensor.py

--- 1Introduction_to_pyTorch/tensors/tensor.py
+++ 1Introduction_to_pyTorch/tensors/tensor.py
@@ -30,10 +30,27 @@ print(f"Random Tensor: \n {rand_tensor} \n")
 print(f"Ones Tensor: \n {ones_tensor} \n")
 print(f"Zeros Tensor: \n {zeros_tensor}")
 
+#######################################################################################################
+# 2: Attributes of a Tensor
+tensor = torch.rand(3,4)
 
+print(f"Shape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dtype}")
+print(f"Device tensor is stored on: {tensor.device}")
 
+#######################################################################################################
+# 3: Operations on Tensors
+# We move our tensor to the GPU if available
+if torch.cuda.is_available():
+    tensor = tensor.to('cuda')
 
-
+# Standard numpy-like indexing and slicing:
+tensor = torch.ones(4, 4)
+print('First row: ', tensor[0])
+print('First column: ', tensor[:, 0])
+print('Last column:', tensor[..., -1])
+tensor[:,1] = 0
+print(tensor)
 
 
 
