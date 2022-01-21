4257c5df47caef7bf12ed9fc4ede9f2ca8db823b
YE GAO <sortsimilar@gmail.com>
2022-01-21 21:16:10 +0800

    Update quickstart.py

--- 1Introduction_to_pyTorch/quickstart/quickstart.py
+++ 1Introduction_to_pyTorch/quickstart/quickstart.py
@@ -130,9 +130,13 @@ classes = [
 
 model.eval()
 x, y = test_data[0][0], test_data[0][1]
+
 with torch.no_grad():
     pred = model(x)
-    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+
+    predicted = classes[pred[0].argmax(0)]
+    actual = classes[y]
+
     print(f'Predicted: "{predicted}", Actual: "{actual}"')
 
 
