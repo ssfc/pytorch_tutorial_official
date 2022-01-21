34f3e111b8405b19f24ec8d7e192bfa03d1eccf8
YE GAO <sortsimilar@gmail.com>
2022-01-21 23:08:26 +0800

    Update tensor.py

--- 1Introduction_to_pyTorch/tensors/tensor.py
+++ 1Introduction_to_pyTorch/tensors/tensor.py
@@ -52,10 +52,51 @@ print('Last column:', tensor[..., -1])
 tensor[:,1] = 0
 print(tensor)
 
+# Joining tensors
+t1 = torch.cat([tensor, tensor, tensor], dim=1)
+print(t1)
+
+# Arithmetic operations
+# This computes the matrix multiplication between two tensors. y1, y2, y3 will have the same value
+y1 = tensor @ tensor.T
+y2 = tensor.matmul(tensor.T)
+
+y3 = torch.rand_like(tensor)
+torch.matmul(tensor, tensor.T, out=y3)
 
 
+# This computes the element-wise product. z1, z2, z3 will have the same value
+z1 = tensor * tensor
+z2 = tensor.mul(tensor)
 
+z3 = torch.rand_like(tensor)
+torch.mul(tensor, tensor, out=z3)
 
+# Single-element tensors
+agg = tensor.sum()
+agg_item = agg.item()
+print(agg_item, type(agg_item))
+
+# In-place operations
+print(tensor, "\n")
+tensor.add_(5)
+print(tensor)
+
+#######################################################################################################
+# 3: Bridge with NumPy
+# Tensor to NumPy array
+t = torch.ones(5)
+print(f"t: {t}")
+n = t.numpy()
+print(f"n: {n}")
+
+# NumPy array to Tensor
+n = np.ones(5)
+t = torch.from_numpy(n)
+
+np.add(n, 1, out=n)
+print(f"t: {t}")
+print(f"n: {n}")
 
 
 
