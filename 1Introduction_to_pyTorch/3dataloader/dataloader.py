867517098f41131722f1b53e8c9bf818c579596d
YE GAO <sortsimilar@gmail.com>
2022-01-22 10:03:46 +0800

    Update dataloader.py

--- 1Introduction_to_pyTorch/3dataloader/dataloader.py
+++ 1Introduction_to_pyTorch/3dataloader/dataloader.py
@@ -47,15 +47,16 @@ class CustomImageDataset(Dataset):
     def __len__(self):
         return len(self.img_labels)
 
+    # The __getitem__ function loads and returns a sample from the dataset at the given index idx. 
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 1]
-        if self.transform:
+        image = read_image(img_path)  # it identifies the image’s location on disk, converts that to a tensor using read_image; 
+        label = self.img_labels.iloc[idx, 1]  # retrieves the corresponding label from the csv data in self.img_labels; 
+        if self.transform:  # calls the transform functions on them (if applicable); 
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
-        return image, label
+        return image, label  # returns the tensor image and corresponding label in a tuple; 
 
 
 
