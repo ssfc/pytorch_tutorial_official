e401c1001089ae437a880e37a6e8326ec4320071
YE GAO <sortsimilar@gmail.com>
2022-01-22 12:29:00 +0800

    Update dataloader.py

--- 1Introduction_to_pyTorch/3dataloader/dataloader.py
+++ 1Introduction_to_pyTorch/3dataloader/dataloader.py
@@ -63,8 +63,20 @@ class CustomImageDataset(Dataset):
 from torch.utils.data import DataLoader
 
 train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)  # pass samples in “minibatches”, reshuffle the data at every epoch; 
-test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)  # DataLoader is an iterable that abstracts this complexity for us in an easy API.
 
+#################################################################################################################################
+# 5: Iterate through the DataLoader
+
+# Display image and label.
+train_features, train_labels = next(iter(train_dataloader))
+print(f"Feature batch shape: {train_features.size()}")
+print(f"Labels batch shape: {train_labels.size()}")
+img = train_features[0].squeeze()
+label = train_labels[0]
+# plt.imshow(img, cmap="gray")
+# plt.show()
+print(f"Label: {label}")
 
 
 
