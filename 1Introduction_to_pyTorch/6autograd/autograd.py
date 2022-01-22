2ede262ed52585926a39d22aa09e2b40822bb9b5
YE GAO <sortsimilar@gmail.com>
2022-01-23 00:30:20 +0800

    Create autograd.py

---
+++ 1Introduction_to_pyTorch/6autograd/autograd.py
@@ -0,0 +1,49 @@
+# https://pytorch.org/tutorials/beginner/basics/autogradqs_tutorial.html
+
+import torch
+
+x = torch.ones(5)  # input tensor
+y = torch.zeros(3)  # expected output, true label; 
+w = torch.randn(5, 3, requires_grad=True)
+b = torch.randn(3, requires_grad=True)
+z = torch.matmul(x, w)+b  # predicted label; 
+loss = torch.nn.functional.binary_cross_entropy_with_logits(z, y)
+
+###################################################################################################
+# 1: Tensors, Functions and Computational graph
+print('Gradient function for z =', z.grad_fn)
+print('Gradient function for loss =', loss.grad_fn)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
