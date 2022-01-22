03fb50d1a8c2928b6c8cde3b37ad30b65afe90d5
YE GAO <sortsimilar@gmail.com>
2022-01-22 18:22:53 +0800

    Update build_model.py

--- 1Introduction_to_pyTorch/5buildnn/build_model.py
+++ 1Introduction_to_pyTorch/5buildnn/build_model.py
@@ -56,7 +56,16 @@ print(f"Before ReLU: {hidden1}\n\n")
 hidden1 = nn.ReLU()(hidden1)  # They are applied after linear transformations to introduce nonlinearity; 
 print(f"After ReLU: {hidden1}")
 
-
+# (4) nn.Sequential
+seq_modules = nn.Sequential(
+    flatten,
+    layer1,
+    nn.ReLU(),
+    nn.Linear(20, 10)
+)
+input_image = torch.rand(3,28,28)
+logits = seq_modules(input_image)
+print("logits size: ", logits.size())
 
 
 
