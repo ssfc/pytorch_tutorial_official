ea0e3ae7ae6e6593b400bd8963144719d8f22369
YE GAO <sortsimilar@gmail.com>
2022-01-23 18:06:17 +0800

    Update save_load.py

--- 1Introduction_to_pyTorch/8save_load/save_load.py
+++ 1Introduction_to_pyTorch/8save_load/save_load.py
@@ -12,6 +12,8 @@ model = models.vgg16() # we do not specify pretrained=True, i.e. do not load def
 model.load_state_dict(torch.load('model_weights.pth'))  # To load model weights, you need to create an instance of the same model first, and then load the parameters using load_state_dict() method.
 model.eval()
 
+###########################################################################################################################################################
+# 2: Saving and Loading Models with Shapes
 
 
 
