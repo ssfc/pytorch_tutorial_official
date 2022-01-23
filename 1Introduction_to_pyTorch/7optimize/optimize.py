4ea8af472c51203f1fc03fd730bf21b5fbbae776
YE GAO <sortsimilar@gmail.com>
2022-01-23 12:35:45 +0800

    Create optimize.py

---
+++ 1Introduction_to_pyTorch/7optimize/optimize.py
@@ -0,0 +1,34 @@
+# https://pytorch.org/tutorials/beginner/basics/optimization_tutorial.html
+
+# (1) makes a guess about the output, 
+# (2) calculates the error in its guess (loss), 
+# (3) collects the derivatives of the error with respect to its parameters (as we saw in the previous section), 
+# (4) and optimizes these parameters using gradient descent; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
