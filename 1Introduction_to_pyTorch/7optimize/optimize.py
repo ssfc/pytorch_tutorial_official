2bfd8a583abc8c900ad0b17d187f9f2d20960d5d
YE GAO <sortsimilar@gmail.com>
2022-01-23 16:34:02 +0800

    Update optimize.py

--- 1Introduction_to_pyTorch/7optimize/optimize.py
+++ 1Introduction_to_pyTorch/7optimize/optimize.py
@@ -59,11 +59,45 @@ epochs = 5  # Each iteration of the optimization loop is called an epoch;
 # 3: Optimization Loop
 # 3.1: Loss Function
 
+# Initialize the loss function
+loss_fn = nn.CrossEntropyLoss()
+
+# 3.2: Optimizer
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+##############################################################################################################################################
+# 4: Full Implementation
+def train_loop(dataloader, model, loss_fn, optimizer):  # We define train_loop that loops over our optimization code; 
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction and loss
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
+def test_loop(dataloader, model, loss_fn):  # and test_loop that evaluates the model’s performance against our test data; 
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
 
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
 
-
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
 
