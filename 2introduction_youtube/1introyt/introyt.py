2ef77b15f5db12ec770b3d37e094f12161f1534b
YE GAO <sortsimilar@gmail.com>
2022-01-23 23:04:55 +0800

    Update introyt.py

--- 2introduction_youtube/1introyt/introyt.py
+++ 2introduction_youtube/1introyt/introyt.py
@@ -122,8 +122,28 @@ print('\nRaw output:')
 print(output)
 print(output.shape)
 
+###############################################################################################################################################################
+# 3: Datasets and Dataloaders
+
+#%matplotlib inline
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),  # transforms.ToTensor() converts images loaded by Pillow into PyTorch tensors. 
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])  # transforms.Normalize() adjusts the values of the tensor so that their average is zero and their standard deviation is 0.5. 
 
 
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)  # organizes the input tensors served by the Dataset into batches with the parameters you specify.
+
+###############################################################################################################################################################
+# 4: Training Your PyTorch Model
 
 
 
