f3b8f68b1f30a0a029bafde9daf3375ada48cdbf
YE GAO <sortsimilar@gmail.com>
2022-01-23 18:05:45 +0800

    Update save_load.py

--- 1Introduction_to_pyTorch/8save_load/save_load.py
+++ 1Introduction_to_pyTorch/8save_load/save_load.py
@@ -6,7 +6,7 @@ import torchvision.models as models
 ###########################################################################################################################################################
 # 1: Saving and Loading Model Weights
 model = models.vgg16(pretrained=True)
-torch.save(model.state_dict(), 'model_weights.pth')
+torch.save(model.state_dict(), 'model_weights.pth')  # PyTorch models store the learned parameters in an internal state dictionary, called state_dict. 
 
 model = models.vgg16() # we do not specify pretrained=True, i.e. do not load default weights
 model.load_state_dict(torch.load('model_weights.pth'))  # To load model weights, you need to create an instance of the same model first, and then load the parameters using load_state_dict() method.
