cdd34d5742be1b23ecc7720e7b942bfcd66890a2
YE GAO <sortsimilar@gmail.com>
2022-01-23 18:12:42 +0800

    Update save_load.py

--- 1Introduction_to_pyTorch/8save_load/save_load.py
+++ 1Introduction_to_pyTorch/8save_load/save_load.py
@@ -10,7 +10,7 @@ torch.save(model.state_dict(), 'model_weights.pth')  # PyTorch models store the 
 
 model = models.vgg16() # we do not specify pretrained=True, i.e. do not load default weights
 model.load_state_dict(torch.load('model_weights.pth'))  # To load model weights, you need to create an instance of the same model first, and then load the parameters using load_state_dict() method.
-model.eval()
+model.eval()  # be sure to call model.eval() method before inferencing to set the dropout and batch normalization layers to evaluation mode. Failing to do this will yield inconsistent inference results.
 
 ###########################################################################################################################################################
 # 2: Saving and Loading Models with Shapes
