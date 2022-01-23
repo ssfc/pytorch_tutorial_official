6610b967eca92d21d16018ac979f8bc3a86ee7cd
YE GAO <sortsimilar@gmail.com>
2022-01-23 19:12:28 +0800

    update

---
+++ 2introduction_youtube/1introyt/introyt.py
@@ -0,0 +1,66 @@
+# https://pytorch.org/tutorials/beginner/introyt/introyt1_tutorial.html
+
+###############################################################################################################################################################
+# 1: PyTorch Tensors
+
+import torch
+
+z = torch.zeros(5, 3)  # Above, we create a 5x3 matrix filled with zeros; 
+print(z)
+print(z.dtype)  # query its datatype to find out that the zeros are 32-bit floating point numbers, which is the default PyTorch; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
