72f5629f5cae10804350132dc0a420ccaa4c0da0
YE GAO <sortsimilar@gmail.com>
2022-01-23 23:38:08 +0800

    Update introyt.py

--- 2introduction_youtube/1introyt/introyt.py
+++ 2introduction_youtube/1introyt/introyt.py
@@ -201,7 +201,7 @@ print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        
+
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
@@ -222,9 +222,34 @@ class Net(nn.Module):
 
 net = Net()
 
+# The last ingredients we need are a loss function and an optimizer:
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
+for epoch in range(2):  # loop over the dataset multiple times
 
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
 
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
 
 
 
