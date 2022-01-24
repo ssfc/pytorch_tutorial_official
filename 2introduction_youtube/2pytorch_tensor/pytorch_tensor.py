a432e682bad2c3eabceb34682a407adf880773dd
YE GAO <sortsimilar@gmail.com>
2022-01-24 23:52:03 +0800

    Update pytorch_tensor.py

--- 2introduction_youtube/2pytorch_tensor/pytorch_tensor.py
+++ 2introduction_youtube/2pytorch_tensor/pytorch_tensor.py
@@ -116,7 +116,57 @@ print(c)
 d = a * torch.rand(   1, 2) # 3rd dim identical to a, 2nd dim = 1
 print(d)
 
+# (2) More Math with Tensors
+# common functions
+a = torch.rand(2, 4) * 2 - 1
+print('Common functions:')
+print(torch.abs(a))
+print(torch.ceil(a))
+print(torch.floor(a))
+print(torch.clamp(a, -0.5, 0.5))
 
+# trigonometric functions and their inverses
+angles = torch.tensor([0, math.pi / 4, math.pi / 2, 3 * math.pi / 4])
+sines = torch.sin(angles)
+inverses = torch.asin(sines)
+print('\nSine and arcsine:')
+print(angles)
+print(sines)
+print(inverses)
+
+# bitwise operations
+print('\nBitwise XOR:')
+b = torch.tensor([1, 5, 11])
+c = torch.tensor([2, 7, 10])
+print(torch.bitwise_xor(b, c))
+
+# comparisons:
+print('\nBroadcasted, element-wise equality comparison:')
+d = torch.tensor([[1., 2.], [3., 4.]])
+e = torch.ones(1, 2)  # many comparison ops support broadcasting!
+print(torch.eq(d, e)) # returns a tensor of type bool
+
+# reductions:
+print('\nReduction ops:')
+print(torch.max(d))        # returns a single-element tensor
+print(torch.max(d).item()) # extracts the value from the returned tensor
+print(torch.mean(d))       # average
+print(torch.std(d))        # standard deviation
+print(torch.prod(d))       # product of all numbers
+print(torch.unique(torch.tensor([1, 2, 1, 2, 1, 2]))) # filter unique elements
+
+# vector and linear algebra operations
+v1 = torch.tensor([1., 0., 0.])         # x unit vector
+v2 = torch.tensor([0., 1., 0.])         # y unit vector
+m1 = torch.rand(2, 2)                   # random matrix
+m2 = torch.tensor([[3., 0.], [0., 3.]]) # three times identity matrix
+
+print('\nVectors & Matrices:')
+print(torch.cross(v2, v1)) # negative of z unit vector (v1 x v2 == -v2 x v1)
+print(m1)
+m3 = torch.matmul(m1, m2)
+print(m3)                  # 3 times m1
+print(torch.svd(m3))       # singular value decomposition
 
 
 
