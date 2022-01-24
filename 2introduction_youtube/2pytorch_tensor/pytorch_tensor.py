795ba24582267b7e3ab7bc445a1e0f18118934e1
YE GAO <sortsimilar@gmail.com>
2022-01-24 19:17:14 +0800

    Update pytorch_tensor.py

--- 2introduction_youtube/2pytorch_tensor/pytorch_tensor.py
+++ 2introduction_youtube/2pytorch_tensor/pytorch_tensor.py
@@ -5,6 +5,11 @@ import math
 
 ##################################################################################################################################################
 # 1: Creating Tensors
+x = torch.empty(3, 4)  # We created a tensor using one of the numerous factory methods attached to the torch module. 
+print(type(x))  # The type of the object returned is torch.Tensor, which is an alias for torch.FloatTensor. 
+print(x)  # The torch.empty() call allocates memory for the tensor, but does not initialize it with any values. 
+
+
 
 
 
