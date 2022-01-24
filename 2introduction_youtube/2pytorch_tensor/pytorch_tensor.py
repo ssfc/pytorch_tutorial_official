e8590de77bf7cdcf79670d62f0a15dd3eeb5aefc
YE GAO <sortsimilar@gmail.com>
2022-01-24 19:10:53 +0800

    Create pytorch_tensor.py

---
+++ 2introduction_youtube/2pytorch_tensor/pytorch_tensor.py
@@ -0,0 +1,33 @@
+# https://pytorch.org/tutorials/beginner/introyt/tensors_deeper_tutorial.html
+
+import torch
+import math
+
+##################################################################################################################################################
+# 1: Creating Tensors
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
