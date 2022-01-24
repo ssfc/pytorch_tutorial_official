0366920943f5915a047f22f515cbcdafbe5f7b64
YE GAO <sortsimilar@gmail.com>
2022-01-24 18:44:09 +0800

    Update introyt.py

--- 2introduction_youtube/1introyt/introyt.py
+++ 2introduction_youtube/1introyt/introyt.py
@@ -1,61 +1,6 @@
 # https://pytorch.org/tutorials/beginner/introyt/introyt1_tutorial.html
 
 ###############################################################################################################################################################
-# 2: PyTorch Models
-import torch                     # for all things PyTorch
-import torch.nn as nn            # for torch.nn.Module, the parent object for PyTorch models
-import torch.nn.functional as F  # for the activation function
-
-
-class LeNet(nn.Module):  # It inherits from torch.nn.Module - modules may be nested - in fact, even the Conv2d and Linear layer classes inherit from torch.nn.Module. 
-
-    def __init__(self):  # A model will have an __init__() function, where it instantiates its layers, and loads any data artifacts it might need (e.g., an NLP model might load a vocabulary).
-        super(LeNet, self).__init__()
-        # 1 input image channel (black & white), 6 output channels, 3x3 square convolution
-        # kernel
-        self.conv1 = nn.Conv2d(1, 6, 3)  # Layer C1 is a convolutional layer, meaning that it scans the input image for features it learned during training. 
-        self.conv2 = nn.Conv2d(6, 16, 3)
-        # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(16 * 6 * 6, 120)  # 6*6 from image dimension
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):  # An input is passed through the network layers and various functions to generate an output. 
-        # Max pooling over a (2, 2) window
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        # If the size is a square you can only specify a single number
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = x.view(-1, self.num_flat_features(x))
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-    def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
-
-# First, we instantiate the LeNet class, and we print the net object. 
-# A subclass of torch.nn.Module will report the layers it has created and their shapes and parameters. 
-# This can provide a handy overview of a model if you want to get the gist of its processing.
-net = LeNet()
-print(net)                         # what does the object tell us about itself?
-
-# Below that, we create a dummy input representing a 32x32 image with 1 color channel. 
-# You may have noticed an extra dimension to our tensor - the batch dimension. 
-input = torch.rand(1, 1, 32, 32)   # stand-in for a 32x32 black & white image
-print('\nImage batch shape:')
-print(input.shape)
-
-output = net(input)                # we don't call forward() directly
-print('\nRaw output:')
-print(output)
-print(output.shape)
-
-###############################################################################################################################################################
 # 3: Datasets and Dataloaders
 
 #%matplotlib inline
