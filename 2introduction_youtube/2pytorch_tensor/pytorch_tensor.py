1adfb0c9e68825d3a66e20ba95a6bbbd6c4bf5f3
YE GAO <sortsimilar@gmail.com>
2022-01-24 19:23:26 +0800

    update

--- 2introduction_youtube/2pytorch_tensor/pytorch_tensor.py
+++ 2introduction_youtube/2pytorch_tensor/pytorch_tensor.py
@@ -9,7 +9,15 @@ x = torch.empty(3, 4)  # We created a tensor using one of the numerous factory m
 print(type(x))  # The type of the object returned is torch.Tensor, which is an alias for torch.FloatTensor. 
 print(x)  # The torch.empty() call allocates memory for the tensor, but does not initialize it with any values. 
 
+zeros = torch.zeros(2, 3)
+print(zeros)
 
+ones = torch.ones(2, 3)
+print(ones)
+
+torch.manual_seed(1729)
+random = torch.rand(2, 3)
+print(random)
 
 
 
