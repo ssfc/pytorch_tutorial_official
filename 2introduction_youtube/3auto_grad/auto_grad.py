aa759b5e5518c40a935fa561415e1b55a448d732
YE GAO <sortsimilar@gmail.com>
2022-01-26 21:18:43 +0800

    Update auto_grad.py

--- 2introduction_youtube/3auto_grad/auto_grad.py
+++ 2introduction_youtube/3auto_grad/auto_grad.py
@@ -85,6 +85,32 @@ model = TinyModel()
 print(model.layer2.weight[0][0:10]) # just a small slice
 print(model.layer2.weight.grad)  # Within a subclass of torch.nn.Module, it’s assumed that we want to track gradients on the layers’ weights for learning. 
 
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+
+prediction = model(some_input)
+
+loss = (ideal_output - prediction).pow(2).sum()
+print(loss)
+
+loss.backward()
+print(model.layer2.weight[0][0:10])  # but the weights remain unchanged, because we haven’t run the optimizer yet. The optimizer is responsible for updating model weights based on the computed gradients. 
+print(model.layer2.weight.grad[0][0:10])  # We can see that the gradients have been computed for each learning weight; 
+
+optimizer.step()
+print(model.layer2.weight[0][0:10])  # You should see that layer2’s weights have changed. 
+print(model.layer2.weight.grad[0][0:10])
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
