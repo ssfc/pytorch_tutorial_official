4462fa461571f0cf39c5c939a01891b2fd499c49
YE GAO <sortsimilar@gmail.com>
2022-01-28 12:24:10 +0800

    Update word_embed.py

--- nlp/3word_embed/word_embed.py
+++ nlp/3word_embed/word_embed.py
@@ -15,9 +15,14 @@ import torch.optim as optim
 
 torch.manual_seed(1)
 
+word_to_ix = {"hello": 0, "world": 1}
+embeds = nn.Embedding(2, 5)  # 2 words in vocab, 5 dimensional embeddings
+lookup_tensor = torch.tensor([word_to_ix["hello"]], dtype=torch.long)
+hello_embed = embeds(lookup_tensor)
+print("hello embedding: ", hello_embed)
 
-
-
+##################################################################################################################################################################
+# 3: An Example: N-Gram Language Modeling
 
 
 
